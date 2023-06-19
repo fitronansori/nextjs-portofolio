3d4807c7e4cfadcc1783d3bb8263a9236d0759db
Fitron Ansori <fitronansori7@gmail.com>
2023-06-19 22:00:00 +0800

    update homepage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,23 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Hero from "public/hero.png";
 
 export default function Home() {
-  return <h1>Hello World</h1>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>
+          Better design for your digital product.
+        </h1>
+        <p className={styles.desc}>
+          Turning your Idea into Reality. We bring together the teams for the
+          global tech industry.
+        </p>
+        <button className={styles.button}>See Our Works</button>
+      </div>
+      <div className={styles.item}>
+        <Image src={Hero} alt="Hero Section" className={styles.img} />
+      </div>
+    </div>
+  );
 }
